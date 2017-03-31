20f4565c8364feecc48636c193ce56adfefc5e07
haro-freezd <harmanmahal@gmail.com>
2017-03-31 02:34:17 -0700

    Added file downloading functionality

--- app/src/main/java/comp3717/bcit/ca/danktank/MainActivity.java
+++ app/src/main/java/comp3717/bcit/ca/danktank/MainActivity.java
@@ -1,13 +1,33 @@
 package comp3717.bcit.ca.danktank;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class MainActivity extends Activity
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+import static android.content.ContentValues.TAG;
+
+public class MainActivity extends Activity implements ActivityCompat.OnRequestPermissionsResultCallback
 {
+    final String pubArtFile = "http://opendata.newwestcity.ca/downloads/public-art/PUBLIC_ART.json";
+    final String artistFile = "http://opendata.newwestcity.ca/downloads/artists/ARTISTS.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,8 +43,99 @@ public class MainActivity extends Activity
 
         Constants.SCREEN_WIDTH = displayMetrics.widthPixels;
         Constants.SCREEN_HEIGHT = displayMetrics.heightPixels;
-
+        if(isNetworkPermissionGranted())
+            new DownloadFileFromURL().execute(pubArtFile, artistFile);
         //// // TODO: 1/24/2017 Create a framelayout to allow UI overlay
         setContentView(new GamePanel(this));
+    }
+
+    private boolean isNetworkPermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.INTERNET)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted");
+                return true;
+            } else {
+
+                Log.v(TAG,"Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, 1);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted");
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            Log.v(TAG,"Permission: "+permissions[0]+ "was "+grantResults[0]);
+            finish();
+            startActivity(getIntent());
+        }
+    }
+
+    private class DownloadFileFromURL extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected String doInBackground(String... f_url) {
+            int count;
+            for(int i = 0; i < f_url.length; i++){
+                try {
+                    String root = Environment.getExternalStorageDirectory().toString();
+
+                    System.out.println("Downloading");
+                    URL url = new URL(f_url[i]);
+
+                    URLConnection conection = url.openConnection();
+                    conection.connect();
+                    // getting file length
+                    int lenghtOfFile = conection.getContentLength();
+
+                    // input stream to read file - with 8k buffer
+                    InputStream input = new BufferedInputStream(url.openStream(), 8192);
+
+                    // Output stream to write file
+                    String fileName = "a" +i+".json";
+                    OutputStream output = openFileOutput(fileName, Context.MODE_PRIVATE);
+                    byte data[] = new byte[1024];
+
+                    long total = 0;
+                    while ((count = input.read(data)) != -1) {
+                        total += count;
+
+                        // writing data to file
+                        output.write(data, 0, count);
+
+                    }
+
+                    // flushing output
+                    output.flush();
+
+                    // closing streams
+                    output.close();
+                    input.close();
+
+                } catch (Exception e) {
+                    Log.e("Error: ", e.getMessage());
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String file_url) {
+
+        }
+
     }
 }
