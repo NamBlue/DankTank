5836653f4dffc0a0dd55b1570db06c6b79b9802e
haro-freezd <harmanmahal@gmail.com>
2017-04-04 22:09:48 -0700

    Combined art manager and powerup manager

--- app/src/main/java/comp3717/bcit/ca/danktank/managers/PowerupManager.java
+++ app/src/main/java/comp3717/bcit/ca/danktank/managers/PowerupManager.java
@@ -2,9 +2,18 @@ package comp3717.bcit.ca.danktank.managers;
 
 import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.util.Log;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
+import comp3717.bcit.ca.danktank.Constants;
 import comp3717.bcit.ca.danktank.objects.Powerup;
 
 /**
@@ -13,12 +22,14 @@ import comp3717.bcit.ca.danktank.objects.Powerup;
 
 public class PowerupManager
 {
-    private ArrayList<Powerup> powerups, collectedPowerups;
+    private ArrayList<Powerup> powerups, collectedPowerups, allPowerUps;
 
     public PowerupManager()
     {
         powerups = new ArrayList<>();
+        allPowerUps = new ArrayList<>();
         collectedPowerups = new ArrayList<>();
+        this.readJsonFromFile();
     }
 
     public boolean collided(Rect object)
@@ -35,9 +46,17 @@ public class PowerupManager
         return false;
     }
 
+    public ArrayList<Powerup> getCollectedPowerups(){
+        return this.collectedPowerups;
+    }
+
     public void spawnPowerup(Rect rect)
     {
-        powerups.add(new Powerup("test", "This is a test object", "www.google.com", rect));
+        int p_index = (int)Math.floor(Math.random() * allPowerUps.size());
+        Powerup p = allPowerUps.get(p_index);
+        Log.d("Power:", p.toString());
+        p.setRectangle(rect);
+        powerups.add(p);
     }
 
     public void draw(Canvas canvas)
@@ -53,6 +72,20 @@ public class PowerupManager
         for(Powerup powerup: powerups)
         {
             powerup.update();
+        }
+    }
+
+    private void readJsonFromFile()
+    {
+        try
+        {
+            Type collectionType = new TypeToken<ArrayList<Powerup>>(){}.getType();
+            String filename = Constants.CURRENT_CONTEXT.getFilesDir() + "/" + "a0.json";
+            JsonReader js = new JsonReader(new FileReader(filename));
+            Gson gson = new Gson();
+            allPowerUps  = gson.fromJson(js, collectionType);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
